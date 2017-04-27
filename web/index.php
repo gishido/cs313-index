b0bc1a1c4e905c285f26d7bba0be6f6c50e1769e
gishido <theshum@gmail.com>
2017-04-26 23:53:16 -0700

    fixed navbar and jumbotron

--- web/index.php
+++ web/index.php
@@ -1,52 +1,72 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>CS313 - Adam Shumway</title>
   <!-- http://getbootstrap.com/getting-started/ - Latest compiled and minified CSS -->
-  <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-  <!-- http://getbootstrap.com/getting-started/ - Latest compiled and minified JavaScript -->
-  <script src="js/bootstrap.min.js"></script>
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <!-- main css file -->
+  <link href="css/main.css" rel="stylesheet" type="text/css">
   <!-- jquery js file...needed for some boostrap functions -->
   <script src="js/jquery-3.2.1.min.js"></script> 
+  <!-- http://getbootstrap.com/getting-started/ - Latest compiled and minified JavaScript -->
+  <script src="js/bootstrap.min.js"></script>
 </head>
 <body>
   <!-- referencing bootstrap's navbar -->
-  <nav class="navbar navbar-default">
-    <div class="container-fluid">
-      <!-- Brand and toggle get grouped for better mobile display -->
-      <div class="navbar-header">
-        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-          <span class="sr-only">Toggle navigation</span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-        <a class="navbar-brand" href="#">Adam Shumway</a>
-      </div>
+  <nav class="navbar navbar-inverse navbar-fixed-top">
+  <div class="container">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Adam Shumway</a>
+    </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+      <ul class="nav navbar-nav navbar-right">
         <li><a href="#">Link</a></li>
+        <li><a href="#">Link</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Assignments <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Assignment 1</a></li>
+            <li><a href="#">Assignment 2</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">One more separated link</a></li>
+          </ul>
+        </li>
       </ul>
     </div><!-- /.navbar-collapse -->
-    </div><!-- /.container-fluid -->
-  </nav>
+  </div><!-- /.container-fluid -->
+</nav>
   
-  <div class="container">
+
     <!-- referencing bootstrap's jumbotron -->
-    <div class="jumbotron">
-      <h1>Hello, world!</h1>
-      <p>This is some sample text that may mean something to someone out there</p>
+    <div class="jumbotron text-center">
+      <div class="container">
+        <h1>CS313</h1>
+        <p id="jumbo"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et illum ullam accusantium esse</p>
+      </div>
     </div>
-  </div>
   <!-- row -->
   <div class="container">
     <div class="row">
       <p>here is some content and you will super love it here</p>
     </div>
   </div>
+  
+
 </body>
 </html>
 
