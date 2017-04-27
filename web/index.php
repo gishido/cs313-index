9d62ea10ecfe5d1a9c9f01a4172888939df24ba3
gishido <theshum@gmail.com>
2017-04-26 22:42:13 -0700

    added basic nav and jumbotron

--- web/index.php
+++ web/index.php
@@ -10,22 +10,43 @@
   <script src="js/jquery-3.2.1.min.js"></script> 
 </head>
 <body>
-<h1>
-  <?php
-    echo "<center>";
-    echo 'Running from Index App';
-    echo "</center>";
-  ?>
-</h1>
-<div class="container" id=button1>
-  <button type="button" class="btn btn-primary hover-bold" onclick="clickMe();">Click Me!</button>
-</div>
-<div class="container" id=button2>
-  <button type="button" class="btn btn-danger hover-bold" onclick="clickMe();">Click Me!</button>
-</div>
-<div class="container" id=button3>
-  <button type="button" class="btn btn-warning hover-bold" onclick="clickMe();">Click Me!</button>
-</div>
+  <!-- referencing bootstrap's navbar -->
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#">Adam Shumway</a>
+      </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+        <li><a href="#">Link</a></li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+  </nav>
+  
+  <div class="container">
+    <!-- referencing bootstrap's jumbotron -->
+    <div class="jumbotron">
+      <h1>Hello, world!</h1>
+      <p>This is some sample text that may mean something to someone out there</p>
+    </div>
+  </div>
+  <!-- row -->
+  <div class="container">
+    <div class="row">
+      <p>here is some content and you will super love it here</p>
+    </div>
+  </div>
 </body>
 </html>
 
