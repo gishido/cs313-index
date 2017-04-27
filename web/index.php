2be4aa72176603cffc6cf0118dc07c5720aab857
gishido <theshum@gmail.com>
2017-04-26 21:59:33 -0700

    getting app setup to work - personal site prep

--- web/index.php
+++ web/index.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>CS313 - Teach02</title>
+  <title>CS313 - Adam Shumway</title>
   <!-- http://getbootstrap.com/getting-started/ - Latest compiled and minified CSS -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <!-- http://getbootstrap.com/getting-started/ - Latest compiled and minified JavaScript -->
-  <script 
-    src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous">
-  </script>
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+  <!-- http://getbootstrap.com/getting-started/ - Latest compiled and minified JavaScript -->
+  <script src="js/bootstrap.min.js"></script>
+  <!-- jquery js file...needed for some boostrap functions -->
+  <script src="js/jquery-3.2.1.min.js"></script> 
 </head>
 <body>
 <h1>
@@ -18,13 +18,13 @@
   ?>
 </h1>
 <div class="container" id=button1>
-  <button type="button" class="btn btn-default hover-bold" onclick="clickMe();">Click Me!</button>
+  <button type="button" class="btn btn-primary hover-bold" onclick="clickMe();">Click Me!</button>
 </div>
 <div class="container" id=button2>
-  <button type="button" class="btn btn-default hover-bold" onclick="clickMe();">Click Me!</button>
+  <button type="button" class="btn btn-danger hover-bold" onclick="clickMe();">Click Me!</button>
 </div>
 <div class="container" id=button3>
-  <button type="button" class="btn btn-default hover-bold" onclick="clickMe();">Click Me!</button>
+  <button type="button" class="btn btn-warning hover-bold" onclick="clickMe();">Click Me!</button>
 </div>
 </body>
 </html>
