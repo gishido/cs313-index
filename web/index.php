e528fd94c91e07f58843631248f48b05c8be9a50
gishido <theshum@gmail.com>
2017-05-06 16:34:05 -0700

    updated shopping.html to shopping.php

--- web/index.php
+++ web/index.php
@@ -37,7 +37,7 @@
         <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Assignments <span class="caret"></span></a>
           <ul class="dropdown-menu">
-            <li><a href="shopping.html">Prove03 - Shopping Cart</a></li>
+            <li><a href="shopping.php">Prove03 - Shopping Cart</a></li>
 <!--            <li><a href="#">Assignment 2</a></li>
             <li><a href="#">Something else here</a></li>
             <li role="separator" class="divider"></li>
