6d5d890111e49d94d3b8475b744203284875ac28
gishido <theshum@gmail.com>
2017-05-18 23:08:39 -0700

    making progress on sections

---
+++ web/stake-search.php
@@ -0,0 +1,64 @@
+<?php
+    session_start();
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+    </head>
+    <body> 
+    <h1>Stake Search</h1>
+        <?php
+
+            // default Heroku Postgres configuration URL
+            $dbUrl = getenv('DATABASE_URL');
+
+            if (empty($dbUrl)) {
+            // example localhost configuration URL with postgres username and a database called cs313db
+            $dbUrl = "postgres://postgres:gishido@localhost:5432/registration";
+            }
+
+            $dbopts = parse_url($dbUrl);
+
+            /* print "<p>$dbUrl</p>\n\n";*/
+
+            $dbHost = $dbopts["host"];
+            $dbPort = $dbopts["port"];
+            $dbUser = $dbopts["user"];
+            $dbPassword = $dbopts["pass"];
+            $dbName = ltrim($dbopts["path"],'/');
+
+            //make connection
+            try {
+                $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+            }
+            catch (PDOException $ex) {
+            print "<p>error: $ex->getMessage() </p>\n\n";
+            die();
+            }
+     
+            //query
+            $sql = 'SELECT ward, stake FROM ward 
+                    JOIN stake ON ward.stakdid = stake.stakeid 
+                    where stake = ?';
+/*            echo $sql.'<br>';*/
+
+            $stake = $_POST['stake'];
+
+            // prepared sql
+            $stmt = $db->prepare($sql);
+            $stmt->bindParam(1, $stake);
+            $stmt->execute();
+            $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            //print to screen
+            echo '<strong>Stake: </strong>'.$stake.'<br><br>';
+            echo '<strong>Wards:</strong><br>';
+            foreach($rows as $row)
+            {
+                echo '&nbsp&nbsp&nbsp'.$row['ward'];
+                echo '<br/>';
+            }
+
+        ?>   
+    </body>
+</html>
